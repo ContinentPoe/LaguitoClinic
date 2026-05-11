53b9b9f3269a4a141e68e6606f3b137bf4b27d6c
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 10:27:56 -0500

    Se crea la clase PriorityEnum para la priridad de pacientes en la clínica

---
+++ src/co/edu/uptc/clinic/enums/PriorityEnum.java
@@ -0,0 +1,25 @@
+package co.edu.uptc.clinic.enums;
+
+/**
+ * <b>Descripción:</b> Enumeración que representa los niveles de prioridad
+ * de un paciente en la cola de atención de la Clínica El Laguito.<br>
+ *
+ * @author Noah Padilla
+ * @version 1
+ */
+
+public enum PriorityEnum {
+ 
+    /** Prioridad baja */
+    LOW(0),
+ 
+    /** Prioridad media */
+    MEDIUM(1),
+ 
+    /** Prioridad alta */
+    HIGH(2),
+ 
+    /** Prioridad crítica */
+    CRITICAL(3);
+
+}
