2944820dca4f660137ba92eefd664ca3383877e6
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 10:37:14 -0500

    Mi commit

--- src/co/edu/uptc/clinic/domain/Patient.java
+++ src/co/edu/uptc/clinic/domain/Patient.java
@@ -13,4 +13,50 @@ public class Patient {
     private String lastName;
     private String email;
 	
+    public Patient() {
+		super();
+	}
+
+	public Patient(Long idPatient, String firstName, String lastName, String email) {
+		super();
+		this.idPatient = idPatient;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public Long getIdPatient() {
+		return idPatient;
+	}
+
+	public void setIdPatient(Long idPatient) {
+		this.idPatient = idPatient;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+    
+    
 }
