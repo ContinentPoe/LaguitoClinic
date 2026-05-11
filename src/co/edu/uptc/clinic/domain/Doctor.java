9d7dde2f9f338bfa0ec9c5d8d18dc3c99d03b155
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 11:00:59 -0500

    Se crean constructores y getters y setters en la clase doctor

--- src/co/edu/uptc/clinic/domain/Doctor.java
+++ src/co/edu/uptc/clinic/domain/Doctor.java
@@ -1,4 +1,5 @@
 package co.edu.uptc.clinic.domain;
+import co.edu.uptc.clinic.enums.*;
 
 public class Doctor {
     private IdentificationTypeEnum identificationType;
@@ -7,6 +8,70 @@ public class Doctor {
     private String lastName;
     private String specialty;
     private int yearsOfExperience;
+	
+    public Doctor() {
+		super();
+	}
 
+	public Doctor(IdentificationTypeEnum identificationType, Long medicalId, String firstName, String lastName,
+			String specialty, int yearsOfExperience) {
+		super();
+		this.identificationType = identificationType;
+		this.medicalId = medicalId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.specialty = specialty;
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	public IdentificationTypeEnum getIdentificationType() {
+		return identificationType;
+	}
+
+	public void setIdentificationType(IdentificationTypeEnum identificationType) {
+		this.identificationType = identificationType;
+	}
+
+	public Long getMedicalId() {
+		return medicalId;
+	}
+
+	public void setMedicalId(Long medicalId) {
+		this.medicalId = medicalId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+    
+    
 	
 }
