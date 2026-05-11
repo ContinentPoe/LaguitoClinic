450178eb5cf555542174614dc5da9862f7ebf3b3
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 10:45:32 -0500

    Mi commit

--- src/co/edu/uptc/clinic/domain/Doctor.java
+++ src/co/edu/uptc/clinic/domain/Doctor.java
@@ -1,5 +1,12 @@
 package co.edu.uptc.clinic.domain;
 
 public class Doctor {
+    private IdentificationTypeEnum identificationType;
+    private Long medicalId;
+    private String firstName;
+    private String lastName;
+    private String specialty;
+    private int yearsOfExperience;
 
+	
 }
