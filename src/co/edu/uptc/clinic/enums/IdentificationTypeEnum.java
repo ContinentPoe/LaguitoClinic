d39380886e3d78bc1db75023707343509657f9f4
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 10:18:27 -0500

    Creación de enum de tipo de identificación

---
+++ src/co/edu/uptc/clinic/enums/IdentificationTypeEnum.java
@@ -0,0 +1,23 @@
+package co.edu.uptc.clinica.enums;
+
+/**
+ * <b>Descripción:</b> Enumeración que representa los tipos de identificación
+ * disponibles en el sistema de la Clínica El Laguito.<br>
+ *
+ * @author Noah Padilla
+ * @version 1
+ */
+public enum IdentificationTypeEnum {
+
+    /** Cédula de ciudadanía */
+    CC("Cédula de ciudadanía"),
+
+    /** Tarjeta de identidad */
+    TI("Tarjeta de identidad"),
+
+    /** Cédula de extranjería */
+    CE("Cédula de extranjería"),
+
+    /** Pasaporte */
+    PA("Pasaporte");
+}
---
+++ src/co/edu/uptc/clinic/ui/Main.java
@@ -0,0 +1,5 @@
+package co.edu.uptc.clinic.ui;
+
+public class Main {
+
+}
