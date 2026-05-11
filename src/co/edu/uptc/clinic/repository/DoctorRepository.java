2202bd9e9e9ef498dad8fbb6e01c5925ae141c94
ContinentPoe <continentpoe1212@gmail.com>
2026-05-11 11:50:33 -0500

    Se crea la clase DoctorRepository junto con el uso de HashSet, métodos Set y la librería de Objects

---
+++ src/co/edu/uptc/clinic/repository/DoctorRepository.java
@@ -0,0 +1,34 @@
+package co.edu.uptc.clinic.repository;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+ 
+import co.edu.uptc.clinic.domain.Doctor;
+import co.edu.uptc.clinic.enums.IdentificationTypeEnum;
+
+public class DoctorRepository {
+	
+	public class DoctorRepository {
+	}
+	    private Set<Doctor> doctors;
+	    
+	    public DoctorRepository() {
+	        this.doctors = new HashSet<>();
+	    }
+
+	    public boolean addDoctor(Doctor doctor) {
+	        return this.doctors.add(doctor);
+	    }
+	    
+	    public Set<Doctor> findAll() {
+	        return new HashSet<>(this.doctors);
+	    }
+	    
+	    public Doctor findByIdentification(IdentificationTypeEnum type, Long medicalId) {
+	        return this.doctors.stream()
+	                .filter(d -> Objects.equals(d.getIdentificationType(), type)
+	                          && Objects.equals(d.getMedicalId(), medicalId))
+	                .findFirst().orElse(null);
+	    }
+
+}
